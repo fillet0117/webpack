1fc11d87b234784f1d70c607b0c415e3236028d5
fillet0117 <asd097477@gmail.com>
2020-08-07 16:58:59 +0800

    insert element ui

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ var config = {
             {
                 test: /\.css$/,
                 loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
             }
         ]
     },
