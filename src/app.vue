8fb9695fbde8b438f30340ffedbb274daeef1238
fillet0117 <asd097477@gmail.com>
2020-08-07 14:02:05 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="message">
+            {{message}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello, vue.js',
+            count: 0
+        }
+    },
+    mounted() {
+        this.handle = setInterval(() => {
+            this.count++
+        }, 1000)
+    },
+    destroyed() {
+        clearInterval(this.handle)
+    }
+}
+</script>
+<style lang="css">
+.message {
+    color: pink;
+    font-size: 1.4em;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack')
+var config = {
+    entry: [
+        'webpack/hot/dev-server',
+        path.join(__dirname, 'src', 'main')
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
+module.exports = config
